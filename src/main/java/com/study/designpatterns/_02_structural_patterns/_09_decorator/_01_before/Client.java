064c0ede0cc2beb93905fb148cab996264613cb3
kadun1 <kadunjj@gmail.com>
2024-06-15 10:35:22 +0900

    design pattenrs : decorator 2

--- src/main/java/com/study/designpatterns/_02_structural_patterns/_09_decorator/_01_before/Client.java
+++ src/main/java/com/study/designpatterns/_02_structural_patterns/_09_decorator/_01_before/Client.java
@@ -16,6 +16,6 @@ public class Client {
         Client client = new Client(new TrimmingCommentService());
         client.writeComment("오징어게임");
         client.writeComment("보는게 하는거 보다 재밌을 수가 없지...");
-
+        client.writeComment("http://naver.com");
     }
 }
