f6e0af81e12400f06e6b079dfdcec14130536883
kadun1 <kadunjj@gmail.com>
2024-06-23 21:45:13 +0900

    degisn patterns : flyweight

---
+++ src/main/java/com/study/designpatterns/_02_structural_patterns/_11_flyweight/_01_before/Client.java
@@ -0,0 +1,12 @@
+package com.study.designpatterns._02_structural_patterns._11_flyweight._01_before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        new Character('h', "white", "kadun", 12);
+        new Character('e', "white", "kadun", 12);
+        new Character('l', "white", "kadun", 12);
+        new Character('l', "white", "kadun", 12);
+        new Character('o', "white", "kadun", 12);
+    }
+}
