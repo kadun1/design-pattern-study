feac54821b9a17ad085e8273ba9744582c90352a
kadun1 <kadunjj@gmail.com>
2024-06-26 22:03:01 +0900

    design patterns : cahin of responsibilities

---
+++ src/main/java/com/study/designpatterns/_03_behavioral_patterns/_13_chain_of_responsibilities/_01_before/Request.java
@@ -0,0 +1,13 @@
+package com.study.designpatterns._03_behavioral_patterns._13_chain_of_responsibilities._01_before;
+
+public class Request {
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public Request(String body) {
+        this.body = body;
+    }
+}
