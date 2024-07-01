7cfdcbc4df062afc2e95b6c581a278f1542f3f76
kadun1 <kadunjj@gmail.com>
2024-07-01 23:45:49 +0900

    design patterns : chain of responsibilities end

--- src/main/java/com/study/designpatterns/_03_behavioral_patterns/_13_chain_of_responsibilities/_03_java/CoRInJava.java
+++ src/main/java/com/study/designpatterns/_03_behavioral_patterns/_13_chain_of_responsibilities/_03_java/CoRInJava.java
@@ -6,11 +6,6 @@ import java.io.IOException;
 
 public class CoRInJava {
     public static void main(String[] args) {
-        Filter filter = new Filter() {
-            @Override
-            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-                chain.doFilter(request, response);
-            }
-        };
+        Filter filter = (request, response, chain) -> chain.doFilter(request, response);
     }
 }
