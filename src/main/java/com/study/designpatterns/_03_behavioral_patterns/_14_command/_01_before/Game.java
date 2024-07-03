0baa56b3ecd2ec16687614f1ccadac39f30ef043
kadun1 <kadunjj@gmail.com>
2024-07-03 19:58:46 +0900

    design patterns : command start

---
+++ src/main/java/com/study/designpatterns/_03_behavioral_patterns/_14_command/_01_before/Game.java
@@ -0,0 +1,7 @@
+package com.study.designpatterns._03_behavioral_patterns._14_command._01_before;
+
+public class Game {
+    public void start() {
+        System.out.println("게임을 시작합니다.");
+    }
+}
